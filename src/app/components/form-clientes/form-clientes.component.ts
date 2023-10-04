3435aa24f437b85b294f457b3204a57f577638a2
esvalen <esvalen@bancolombia.com.co>
2023-10-04 16:46:14 -0500

    Implementacion formularios categoria y clientes

--- src/app/components/form-clientes/form-clientes.component.ts
+++ src/app/components/form-clientes/form-clientes.component.ts
@@ -4,14 +4,32 @@ import { FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
-  selector: 'app-formulario',
-  templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  selector: 'app-form-clientes',
+  templateUrl: './form-clientes.component.html',
+  styleUrls: ['./form-clientes.component.css']
 })
-export class FormularioComponent {
+export class FormClientesComponent {
   private fb = inject(FormBuilder);
   addressForm = this.fb.group({
-    categoria : [null,Validators.required]
+    id: [null,Validators.compose([
+      Validators.required,Validators.pattern('^[0-9]+$')
+    ])],
+    usuario:[null,Validators.compose([
+      Validators.required,Validators.pattern('^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$')
+    ])],
+    nombre: [null,Validators.required],
+    nombre2:null,
+    apellido:[null,Validators.required],
+    apellido2:[null,Validators.required],
+    telefono: [null,Validators.compose([
+      Validators.required,Validators.pattern('^[0-9]+$')
+    ])],
+    edad: [null,Validators.compose([
+      Validators.required,Validators.pattern('^[0-9]+$')
+    ])],
+    correo: [null,Validators.compose([
+      Validators.required,Validators.pattern('/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/')
+    ])],
   });
 
   hasUnitNumber = false;
